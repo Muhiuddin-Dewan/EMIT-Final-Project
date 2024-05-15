fe09bf3d274129d0f7ca3a9dae350bbcba93a1de
Muhiuddin-Dewan <mohidewan10@gmail.com>
2024-05-15 15:56:13 +0600

    Logout functionality added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Layout from '../components/Layouts/Layout'
-
+import React from "react";
+import Layout from "../components/Layouts/Layout";
+import { useAuth } from "../context/auth";
 const Home = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout>
-        <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
